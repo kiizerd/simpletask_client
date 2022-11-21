0aad877eff6ef9772a9c21dbe649c3ab2b0c7509
kiizerd <ajsewell13@gmail.com>
2022-11-20 20:23:34 -0600

    Added default Button styles.

--- src/Mantine.tsx
+++ src/Mantine.tsx
@@ -1,9 +1,20 @@
-import { MantineProvider } from "@mantine/core";
 import React from "react";
+import { MantineProvider, ButtonProps } from "@mantine/core";
+
+const ButtonDefaultProps: Partial<ButtonProps> = {
+  p: 'xs',
+  color: "dark",
+  variant: "white",
+};
 
 const MantineApp = ({ children }: React.PropsWithChildren) => (
   <MantineProvider
-    theme={{ colorScheme: "dark" }}
+    theme={{
+      colorScheme: "dark",
+      components: {
+        Button: { defaultProps: ButtonDefaultProps }
+      }
+    }}
     withGlobalStyles
     withNormalizeCSS
   >
