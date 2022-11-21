75079b774a2f5b113c656da4f7022a971dae1e15
kiizerd <ajsewell13@gmail.com>
2022-11-20 20:26:58 -0600

    Unified return values, accepted parameters, and types across Project Model api.

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,7 +6,12 @@ const projectsRoute = apiURL + "/projects";
 
 const getProject = async (id: number): Promise<Project> => {
   const response = await fetch(projectsRoute + `/${id}`);
-  const project = await response.json();
+  const data = await response.json();
+  const project: Project = {
+    id: data.id,
+    title: data.title,
+    description: data.description,
+  };
 
   return project;
 };
@@ -14,21 +19,67 @@ const getProject = async (id: number): Promise<Project> => {
 const getAllProjects = async (): Promise<Project[]> => {
   const response = await fetch(projectsRoute);
   const data = await response.json();
+  const projects = data.map((dataItem: Project) => ({
+    id: dataItem.id,
+    title: dataItem.title,
+    description: dataItem.description,
+  }));
 
-  return data;
+  return projects;
 };
 
-const newProject = async (title: string, description: string): Promise<Project> => {
+const createProject = async (
+  projectData: Partial<Project>
+): Promise<Project> => {
+  const { description, title } = projectData;
   const request = {
     method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    body : JSON.stringify({ project: { title, description} }),
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ project: { title, description } }),
   };
   const response = await fetch(projectsRoute, request);
   const data = await response.json();
-  const project = { ...data };
+  const project: Project = {
+    id: data.id,
+    title: data.title,
+    description: data.description,
+  };
 
   return project;
 };
 
-export { getProject, getAllProjects, newProject };
+const updateProject = async (oldProject: Project): Promise<Project> => {
+  const { id, description, title } = oldProject;
+  const projectRoute = projectsRoute + `/${id}`;
+  const request = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ project: { title, description } }),
+  };
+  const response = await fetch(projectRoute, request);
+  const data = await response.json();
+  const project: Project = {
+    id: data.id,
+    title: data.title,
+    description: data.description,
+  };
+
+  return project;
+};
+
+const deleteProject = async (projectId: number): Promise<number> => {
+  const request = { method: "DELETE" };
+  const projectRoute = projectsRoute + `/${projectId}`;
+  const response = await fetch(projectRoute, request);
+  // const data = await response.json();
+
+  return response.status;
+};
+
+export {
+  getProject,
+  getAllProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+};
