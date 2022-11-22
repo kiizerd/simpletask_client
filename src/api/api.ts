7df71b242a86bc2aa9746e68a63ce7571c4bf4cf
kiizerd <ajsewell13@gmail.com>
2022-11-22 00:46:15 -0600

    Made old file an aggregate of other files.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,85 +1,21 @@
-import { Project } from "../types/models";
+import * as Project from "./projects";
+import * as Task from "./tasks";
+import * as Section from "./sections";
 
-// const apiURL = import.meta.env.API_URL;
-const apiURL = "http://localhost:3000";
-const projectsRoute = apiURL + "/projects";
+export const getAllProjects = Project.getAllProjects;
+export const getProject = Project.getProject;
+export const createProject = Project.createProject;
+export const updateProject = Project.updateProject;
+export const deleteProject = Project.deleteProject;
 
-const getProject = async (id: number): Promise<Project> => {
-  const response = await fetch(projectsRoute + `/${id}`);
-  const data = await response.json();
-  const project: Project = {
-    id: data.id,
-    title: data.title,
-    description: data.description,
-  };
+export const getProjectTasks = Task.getProjectTasks;
+export const getProjectTask = Task.getProjectTask;
+export const createProjectTask = Task.createProjectTask;
+export const updateProjectTask = Task.updateProjectTask;
+export const deleteProjectTask = Task.deleteProjectTask;
 
-  return project;
-};
-
-const getAllProjects = async (): Promise<Project[]> => {
-  const response = await fetch(projectsRoute);
-  const data = await response.json();
-  const projects = data.map((dataItem: Project) => ({
-    id: dataItem.id,
-    title: dataItem.title,
-    description: dataItem.description,
-  }));
-
-  return projects;
-};
-
-const createProject = async (
-  projectData: Partial<Project>
-): Promise<Project> => {
-  const { description, title } = projectData;
-  const request = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ project: { title, description } }),
-  };
-  const response = await fetch(projectsRoute, request);
-  const data = await response.json();
-  const project: Project = {
-    id: data.id,
-    title: data.title,
-    description: data.description,
-  };
-
-  return project;
-};
-
-const updateProject = async (oldProject: Project): Promise<Project> => {
-  const { id, description, title } = oldProject;
-  const projectRoute = projectsRoute + `/${id}`;
-  const request = {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ project: { title, description } }),
-  };
-  const response = await fetch(projectRoute, request);
-  const data = await response.json();
-  const project: Project = {
-    id: data.id,
-    title: data.title,
-    description: data.description,
-  };
-
-  return project;
-};
-
-const deleteProject = async (projectId: number): Promise<number> => {
-  const request = { method: "DELETE" };
-  const projectRoute = projectsRoute + `/${projectId}`;
-  const response = await fetch(projectRoute, request);
-  // const data = await response.json();
-
-  return response.status;
-};
-
-export {
-  getProject,
-  getAllProjects,
-  createProject,
-  updateProject,
-  deleteProject,
-};
+export const getProjectSections = Section.getProjectSections;
+export const getProjectSection = Section.getProjectSection;
+export const createProjectSection = Section.createProjectSection;
+export const updateProjectSection = Section.updateProjectSection;
+export const deleteProjectSection = Section.deleteProjectSection;
