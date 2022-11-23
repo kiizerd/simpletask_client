a8c7da6a26a9a065230325ccd21493b07917a35a
kiizerd <ajsewell13@gmail.com>
2022-11-22 22:41:37 -0600

    Add Section interface and added ids to other models.

--- src/types/models.d.ts
+++ src/types/models.d.ts
@@ -1,12 +1,20 @@
 interface Project {
+  id: number;
   title: string;
   description: string;
+}
+
+interface Section {
   id: number;
+  name: string;
 }
 
 interface Task {
+  id: number;
   name: string;
   details: string;
+  projectId: number;
+  sectionId: number;
 }
 
-export { Project, Task };
+export { Project, Section, Task };
