138f861faa6160f476a50f8ac5b7a09d88eab42a
kiizerd <ajsewell13@gmail.com>
2022-11-22 23:10:29 -0600

    Added SimpleGrid to display SectionCard components for projects associated sections.

--- src/pages/ShowProject.tsx
+++ src/pages/ShowProject.tsx
@@ -1,33 +1,79 @@
-import { Button, Container, Group, Text, Title } from "@mantine/core"
-import { useEffect, useState } from "react"
-import { useLoaderData } from "react-router-dom"
-import { getProject } from "../api/api"
-import { Project } from "../types/models"
+import {
+  Box,
+  Button,
+  Container,
+  Group,
+  SimpleGrid,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getProject, getProjectSections } from "../api/api";
+import { Project, Section } from "../types/models";
+import SectionCard from "../components/SectionCard";
 
 const ShowProjectPage = () => {
-  const id = Number(useLoaderData())
-  const [project, setProject] = useState<Project>()
+  const id = Number(useLoaderData());
+  const [project, setProject] = useState<Project>();
+  const [sections, setSections] = useState<Section[]>([]);
 
-  useEffect(() => {    
-    getAndSetProject()
-  }, [])
+  useEffect(() => {
+    getAndSetProject();
+    getAndSetSections();
+  }, []);
+
+  useEffect(() => {
+    getAndSetSections();
+  }, [project]);
 
   const getAndSetProject = async () => {
-    setProject(await getProject(id))
-  }
+    setProject(await getProject(id));
+  };
 
-  if (!project) return <></>
-  return <Container>
-    <Group position="apart">
-      <Title>{project.title}</Title>
-      <Group>
-        <Button>Edit</Button>
-        <Button>Delete</Button>
+  const getAndSetSections = async () => {
+    setSections(await getProjectSections(id));
+  };
+
+  if (!project) return <></>;
+  return (
+    <Container>
+      <Group position="apart">
+        <Title>{project.title}</Title>
+        <Group>
+          <Button>Edit</Button>
+          <Button>Delete</Button>
+        </Group>
       </Group>
-    </Group>
-    <Text>{project.description}</Text>
+      <Text>{project.description}</Text>
 
-  </Container>
-}
+      <hr />
+      {/* <Title order={3}>Tasks</Title> */}
+      <SimpleGrid
+        cols={4}
+        spacing="lg"
+        breakpoints={[
+          { maxWidth: 980, cols: 3, spacing: "md" },
+          { maxWidth: 755, cols: 2, spacing: "sm" },
+          { maxWidth: 600, cols: 1, spacing: "sm" },
+        ]}
+      >
+        {sections.map((section) => (
+          <Box key={section.id} sx={{ transition: 'height 0.3s linear' }}>
+            <SectionCard
+              projectId={id}
+              section={section}
+              update={getAndSetSections}
+            />
+          </Box>
+        ))}
+        {/* --- */}
+        <Box>
+          <SectionCard projectId={id} update={getAndSetSections} />
+        </Box>
+      </SimpleGrid>
+    </Container>
+  );
+};
 
-export default ShowProjectPage
+export default ShowProjectPage;
