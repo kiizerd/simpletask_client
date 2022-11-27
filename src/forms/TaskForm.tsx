a26ee0c5032dea96cc29b42e52bd51f765b7f88a
kiizerd <ajsewell13@gmail.com>
2022-11-27 16:23:15 -0600

    Made TaskForm match other forms.

--- src/forms/TaskForm.tsx
+++ src/forms/TaskForm.tsx
@@ -1,38 +1,63 @@
-import { ActionIcon, Button, Flex, TextInput } from "@mantine/core";
+import { ActionIcon, Box, Flex, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { IconPlus } from "@tabler/icons";
 import { useLoaderData } from "react-router-dom";
 import { createProjectTask } from "../api/tasks";
+import { Task } from "../types/models";
 
 interface TaskFormProps {
   sectionId: number;
-  update(): void;
+  add(newTask: Task): void;
 }
 
 interface TaskFormValues {
   name: string;
 }
 
-const TaskForm = ({ sectionId, update }: TaskFormProps) => {
+const nameValidation = (name: string) => {
+  if (name && name !== "" && name.length > 3 && name.length < 24) return null;
+
+  if (!name || name === "") return "Name is required.";
+  if (name.length < 3) return "Name must be at 3 chars.";
+  if (name.length > 24) return "Name cannot exceed 24 chars.";
+};
+
+const TaskForm = ({ sectionId, add }: TaskFormProps) => {
   const projectId = Number(useLoaderData());
   const form = useForm({
     initialValues: { name: "" },
-    validate: { name: (value) => (value.length > 0 ? null : "Name required") },
+    validate: { name: nameValidation },
   });
 
   const submit = async (formValues: TaskFormValues) => {
-    await createProjectTask(projectId, { ...formValues, sectionId });
+    const taskData = { sectionId, ...formValues };
+    const newTask = await createProjectTask(projectId, taskData);
+
+    if (!newTask) return;
+
     form.setValues({ name: "" });
-    update();
+    add(newTask);
   };
 
+  const errorTimeout = () => setTimeout(() => form.clearErrors(), 3000);
   return (
     <form onSubmit={form.onSubmit(submit)}>
       <Flex gap="sm">
-        <TextInput placeholder="New task" {...form.getInputProps("name")} />
-        <Button type="submit">
-          <IconPlus />
-        </Button>
+        <TextInput
+          w="100%"
+          placeholder="New task"
+          {...form.getInputProps("name")}
+        />
+        <Box pt={4}>
+          <ActionIcon
+            variant="filled"
+            color="violet"
+            type="submit"
+            onClick={errorTimeout}
+          >
+            <IconPlus />
+          </ActionIcon>
+        </Box>
       </Flex>
     </form>
   );
