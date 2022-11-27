722b7ce3b8356ea0ecf41afe1c010e3972271d07
kiizerd <ajsewell13@gmail.com>
2022-11-27 15:48:49 -0600

    Moved header styles to external file.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import {
-  createStyles,
   Header,
   Container,
   Group,
@@ -11,91 +10,7 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Link, useLocation } from "react-router-dom";
-
-const HEADER_HEIGHT = 60;
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    position: "relative",
-    zIndex: 100,
-    marginBottom: "1rem",
-    [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
-      marginBottom: "2rem",
-    },
-  },
-
-  dropdown: {
-    position: "absolute",
-    top: HEADER_HEIGHT,
-    left: 0,
-    right: 0,
-    zIndex: 0,
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopWidth: 0,
-    overflow: "hidden",
-
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  header: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "100%",
-  },
-
-  links: {
-    [theme.fn.smallerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "8px 12px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-
-    [theme.fn.smallerThan("sm")]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
-    },
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-        .color,
-    },
-  },
-}));
+import headerStyles, { HEADER_HEIGHT } from "./styles/HeaderStyles";
 
 interface HeaderResponsiveProps {
   links: { link: string; label: string }[];
@@ -104,7 +19,7 @@ interface HeaderResponsiveProps {
 const HeaderResponsive = ({ links }: HeaderResponsiveProps) => {
   const [opened, { toggle, close }] = useDisclosure(false);
   const [active, setActive] = useState("");
-  const { classes, cx } = useStyles();
+  const { classes, cx } = headerStyles();
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -133,6 +48,7 @@ const HeaderResponsive = ({ links }: HeaderResponsiveProps) => {
         <Link to="/" style={{ all: "unset", cursor: "pointer" }}>
           <Title order={3}>SimpleTask</Title>
         </Link>
+
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
