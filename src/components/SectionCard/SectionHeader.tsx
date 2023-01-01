3f227b000b63a7fddce20bae6778ea8d800f7511
kiizerd <ajsewell13@gmail.com>
2023-01-01 12:03:18 -0600

    Display Progress of Section in card header.

--- src/components/SectionCard/SectionHeader.tsx
+++ src/components/SectionCard/SectionHeader.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { ActionIcon, Group, Title } from "@mantine/core";
+import { ActionIcon, Box, Group, Progress, Title } from "@mantine/core";
 import { IconEdit } from "@tabler/icons";
 import EditSectionForm from "../../forms/EditSectionForm";
 import sectionCardStyles from "../../styles/SectionCardStyles";
@@ -7,32 +7,37 @@ import SectionHeaderMenu from "./SectionHeaderMenu";
 import SectionContext from "../../contexts/SectionContext";
 
 const SectionCardHeader = () => {
-  const { name } = useContext(SectionContext);
   const [editMode, setEditMode] = useState<boolean>();
+  const { name, progress } = useContext(SectionContext);
   const { classes } = sectionCardStyles();
 
-  return editMode ? (
-    <EditSectionForm setEditMode={setEditMode} />
-  ) : (
-    <Group position="apart" className={classes.header}>
-      {/* Prevent animation on first render */}
-      <Title
-        className={editMode === undefined ? "" : classes.title}
-        order={5}
-        style={{ cursor: "pointer" }}
-        onClick={() => setEditMode(true)}
-      >
-        {name}
-      </Title>
+  return (
+    <Box>
+      {editMode ? (
+        <EditSectionForm setEditMode={setEditMode} />
+      ) : (
+        <Group position="apart" className={classes.header}>
+          {/* Prevent animation on first render */}
+          <Title
+            className={editMode === undefined ? "" : classes.title}
+            order={5}
+            style={{ cursor: "pointer" }}
+            onClick={() => setEditMode(true)}
+          >
+            {name}
+          </Title>
 
-      <Group spacing="sm" my={2}>
-        <ActionIcon color="blue" onClick={() => setEditMode(true)}>
-          <IconEdit size={16} />
-        </ActionIcon>
+          <Group spacing="sm" my={2}>
+            <ActionIcon color="blue" onClick={() => setEditMode(true)}>
+              <IconEdit size={16} />
+            </ActionIcon>
 
-        <SectionHeaderMenu />
-      </Group>
-    </Group>
+            <SectionHeaderMenu />
+          </Group>
+        </Group>
+      )}
+      <Progress size="xs" value={progress} />
+    </Box>
   );
 };
 
