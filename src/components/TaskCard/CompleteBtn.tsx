ae7e647e019767c3ef016cfb5ea97e45d2d548a8
kiizerd <ajsewell13@gmail.com>
2022-12-31 23:41:34 -0600

    Created Complete Button to toggle completion status of Tasks.

---
+++ src/components/TaskCard/CompleteBtn.tsx
@@ -0,0 +1,38 @@
+import { ActionIcon } from "@mantine/core";
+import { IconCheck, IconRefresh } from "@tabler/icons";
+import { useState } from "react";
+import { KeyedMutator, mutate as globalMutate } from "swr";
+import { updateProjectTask } from "../../api/tasks";
+import useTask from "../../hooks/useTask";
+import Task from "../../types/Task";
+
+interface CompleteBtnProps {
+  task: Task;
+}
+
+const toggleCompletion = async (
+  task: Task,
+  mutate: KeyedMutator<Task>,
+  setter: (value: boolean) => void
+) => {
+  setter(task.toggleCompletion() === "complete");
+  const newTask = await updateProjectTask(task.projectId, task);
+  await mutate(newTask);
+  await globalMutate(`projects/${task.projectId}/sections/${task.sectionId}`);
+};
+
+const CompleteBtn = ({ task }: CompleteBtnProps) => {
+  const [isComplete, setIsComplete] = useState<boolean>(task.isComplete());
+  const { mutate } = useTask(task.projectId, task.id);
+
+  return (
+    <ActionIcon
+      color="green"
+      onClick={() => toggleCompletion(task, mutate, setIsComplete)}
+    >
+      {isComplete ? <IconRefresh size={18} /> : <IconCheck size={18} />}
+    </ActionIcon>
+  );
+};
+
+export default CompleteBtn;
