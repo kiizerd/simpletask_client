27a555730099b91a419b2cbf6b53c71a9e2ad0fa
kiizerd <ajsewell13@gmail.com>
2022-12-31 23:43:30 -0600

    Added status property and helpers to Base class.

--- src/types/Base.ts
+++ src/types/Base.ts
@@ -1,22 +1,49 @@
+import { Status } from "./types";
+
 export interface BaseData {
   id: number;
+  status: Status;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export default class Base implements BaseData {
   id: number;
+  status: Status;
   createdAt: Date;
   updatedAt: Date;
 
   constructor(id: number, data: Partial<Base | BaseData>) {
-    const { createdAt = new Date(), updatedAt = new Date() } = data;
+    const {
+      status = "unknown",
+      createdAt = new Date(),
+      updatedAt = new Date(),
+    } = data;
     this.id = id;
+    this.status = status;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
 
   isValid() {
     return this.id != 0;
+  }
+
+  isComplete() {
+    return this.status === "complete";
+  }
+
+  isActive() {
+    return this.status === "active";
+  }
+
+  toggleCompletion() {
+    if (this.isComplete()) {
+      this.status = "standby";
+    } else {
+      this.status = "complete";
+    }
+
+    return this.status;
   }
 }
