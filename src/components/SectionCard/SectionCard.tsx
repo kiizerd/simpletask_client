b7410a3b482cfcea8cc40139d878c1a8c1641ed9
kiizerd <ajsewell13@gmail.com>
2023-01-01 12:36:27 -0600

    Used hooks less stupidly.

--- src/components/SectionCard/SectionCard.tsx
+++ src/components/SectionCard/SectionCard.tsx
@@ -3,12 +3,16 @@ import SectionCardHeader from "./SectionHeader";
 import Section from "../../types/Section";
 import SectionContext from "../../contexts/SectionContext";
 import TaskList from "../TaskList/TaskList";
+import useSection from "../../hooks/useSection";
 
 export interface SectionCardProps {
   section: Section;
 }
 
-const SectionCard = ({ section }: SectionCardProps) => {
+const SectionCard = (props: SectionCardProps) => {
+  const { id, projectId } = props.section;
+  const { section = props.section } = useSection(projectId, id);
+
   return (
     <Card p="xs">
       <SectionContext.Provider value={section}>
