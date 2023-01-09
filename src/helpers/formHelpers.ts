c9af92cba575fbafd0ddb8fbb2981a77cd26972e
kiizerd <ajsewell13@gmail.com>
2023-01-09 12:38:17 -0600

    Extracted form helper methods for special textarea behavior.

--- src/helpers/formHelpers.ts
+++ src/helpers/formHelpers.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface FormWithErrors {
   clearErrors(): void;
 }
@@ -5,3 +7,17 @@ interface FormWithErrors {
 export const errorTimeout = (form: FormWithErrors, timeout?: number) => {
   setTimeout(() => form.clearErrors(), timeout ? timeout : 5000)
 };
+
+export const flushTextAreaCursor = (e: React.FocusEvent<HTMLTextAreaElement, HTMLElement>) => {
+  const len = e.currentTarget.value.length;
+  e.currentTarget.setSelectionRange(len, len);
+}
+
+export const preventTextAreaSubmitOnEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  if (e.key == "Enter" && !e.shiftKey) {
+    const target = e.target as HTMLTextAreaElement;
+    const form = target.form;
+    e.preventDefault();
+    form?.requestSubmit();
+  }
+}
