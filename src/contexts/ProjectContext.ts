9ac19162574a0980a6a0cdcaa53feb43c2291bbf
kiizerd <ajsewell13@gmail.com>
2023-01-10 12:24:02 -0600

    Deleted safety net files now that all modules are working properly.

--- src/contexts/ProjectContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import Project from "../types/Project";
-
-const ProjectContext = createContext({} as Project);
-export default ProjectContext;
--- src/contexts/SectionContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import Section from "../types/Section";
-
-const defaultSection = new Section(0, {});
-const SectionContext = createContext(defaultSection);
-export default SectionContext;
