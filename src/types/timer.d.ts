f4dc02a0ad78fc9972d47c79d55181a9173dc923
kiizerd <ajsewell13@gmail.com>
2023-01-18 18:37:24 -0600

    Wrote types and interfaces for Timer.

---
+++ src/types/timer.d.ts
@@ -0,0 +1,43 @@
+import { Dispatch } from "react";
+
+interface TimerCompletions {
+  work: number;
+  long: number;
+  short: number;
+}
+
+type TimerLength = 5 | 15 | 25;
+
+export interface TimerState {
+  time: number;
+  timerLength: TimerLength;
+  isPaused: boolean;
+  isStarted: boolean;
+  gradientAngle: number;
+  completions: TimerCompletions;
+}
+
+export interface Action {
+  type: string;
+  payload?: number;
+}
+
+export interface TimerController {
+  start(d: Dispatch<Action>): void;
+  pause(d: Dispatch<Action>): void;
+  resume(d: Dispatch<Action>): void;
+  reset(d: Dispatch<Action>): void;
+  set(d: Dispatch<Action>, v: number): void;
+  tick(d: Dispatch<Action>): void;
+  angle(d: Dispatch<Action>): void;
+  complete(d: Dispatch<Action>, v: number): void;
+  reducer(s: TimerState, a: Action): TimerState;
+  initialState: TimerState;
+}
+
+export interface Timer {
+  state: TimerState;
+  dispatch: Dispatch<Action>;
+  controller: TimerController;
+  classes: Record<"wrapper" | "box" | "timer", string>;
+}
