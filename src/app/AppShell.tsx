347a48111a97ee46e15a522905e20db5580ef424
kiizerd <ajsewell13@gmail.com>
2023-05-31 02:23:12 -0500

    Integrated useAuth hook with top level UserContext value.

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -4,22 +4,27 @@ import MantineApp from "./components/Mantine";
 import Header from "./components/Header";
 import TimerContext from "@contexts/TimerContext";
 import useTimer from "@hooks/useTimer";
+import UserContext from "@contexts/UserContext";
+import useAuth from "@hooks/useAuth";
 
-const App = () => {
+const App = (): JSX.Element => {
   // Track timer data in top level component to ensure
   // accurate time as traversing the app
   const timerData = useTimer();
+  const authData = useAuth();
 
   return (
     <MantineApp>
-      <AppShell
-        header={<Header />}
-        styles={{ main: { paddingLeft: 0, paddingRight: 0 } }}
-      >
-        <TimerContext.Provider value={timerData}>
-          <Outlet />
-        </TimerContext.Provider>
-      </AppShell>
+      <UserContext.Provider value={authData}>
+        <AppShell
+          header={<Header />}
+          styles={{ main: { paddingLeft: 0, paddingRight: 0 } }}
+        >
+          <TimerContext.Provider value={timerData}>
+            <Outlet />
+          </TimerContext.Provider>
+        </AppShell>
+      </UserContext.Provider>
     </MantineApp>
   );
 };
