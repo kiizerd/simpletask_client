0afac5f7802f373e0899f42bed88a6606155a9c7
kiizerd <ajsewell13@gmail.com>
2023-05-31 02:14:25 -0500

    Refactor useAuth with useState and navigation.

--- src/modules/hooks/useAuth.tsx
+++ src/modules/hooks/useAuth.tsx
@@ -1,30 +1,62 @@
 import { signIn, signUp, signOut } from "@api/auth";
 import { useLocalStorage } from "@mantine/hooks";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { mutate } from "swr";
 import type { User } from "types/models";
 
-export default function useAuth() {
-  const [currentUser, setCurrentUser] = useLocalStorage<User | null>({
-    key: "current_user",
-    defaultValue: null,
+export interface AuthData {
+  currentUser: User | undefined;
+  login: (email: string, password: string) => Promise<User | undefined>;
+  logout: () => Promise<void>;
+  register: (email: string, password: string) => Promise<User | undefined>;
+  clearCache: () => Promise<void>;
+}
+
+export default function useAuth(): AuthData {
+  const navigate = useNavigate()
+  const [user, setUser] = useState<User | undefined>(undefined)
+  const [currentUser, setCurrentUser, removeValue] = useLocalStorage<
+    User | undefined
+  >({
+    key: "currentUser",
+    defaultValue: undefined,
   });
 
-  const login = async (email: string, password: string) => {
+  useEffect(() => {
+    setUser(currentUser)
+  }, [currentUser])
+
+  const login = async (
+    email: string,
+    password: string
+  ): Promise<User | undefined> => {
+    await clearCache();
     const user = await signIn(email, password);
-    setCurrentUser(user || null);
-    console.log("Logging in from Auth hook")
+    setCurrentUser(user);
+    navigate("/projects")
+    return user;
   };
 
-  const logout = async () => {
+  const logout = async (): Promise<void> => {
     await signOut();
-    setCurrentUser(null);
-    console.log("Logging out from Auth hook")
+    await clearCache();
+    navigate("/welcome")
   };
 
-  const register = async (email: string, password: string) => {
+  const register = async (
+    email: string,
+    password: string
+  ): Promise<User | undefined> => {
     const user = await signUp(email, password);
-    setCurrentUser(user || null);
-    console.log("Signing up from Auth hook")
+    setCurrentUser(user);
+    return user;
   };
 
-  return { currentUser, login, logout, register };
+  const clearCache = async (): Promise<void> => {
+    removeValue();
+    await mutate(() => true, undefined, { revalidate: false });
+  };
+
+  return { currentUser: user, login, logout, register, clearCache };
 }
