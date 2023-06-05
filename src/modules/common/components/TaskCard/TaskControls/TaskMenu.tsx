0f3c68c67482739b46e8aa614a405057ffae588b
kiizerd <ajsewell13@gmail.com>
2023-06-05 16:35:20 -0500

    Handled api errors for Task DELETE in TaskMenu.

--- src/modules/common/components/TaskCard/TaskControls/TaskMenu.tsx
+++ src/modules/common/components/TaskCard/TaskControls/TaskMenu.tsx
@@ -9,48 +9,39 @@ import {
   IconX,
 } from "@tabler/icons";
 import { deleteProjectTask } from "@api/tasks";
-import { getProjectSection } from "@api/sections";
 import TaskIndexContext from "@contexts/TaskIndexContext";
-import useMatchMutate from "@hooks/useMatchMutate";
 import TaskModal from "./TaskModal";
-import Task from "types/Task";
+import type Task from "types/Task";
 
 interface TaskMenuProps {
   task: Task;
   opened: boolean;
-  setOpened(value: boolean): void;
-  setEditMode(value: boolean): void;
+  setOpened: (value: boolean) => void;
+  setEditMode: (value: boolean) => void;
 }
 
-const TaskMenu = (props: TaskMenuProps) => {
+const TaskMenu = (props: TaskMenuProps): JSX.Element => {
   const { task, opened, ...setters } = props;
   const { id, projectId } = task;
   const { setOpened, setEditMode } = setters;
   const [confirmDelete, setConfirmDelete] = useState<boolean>();
   const [modalOpened, setModalOpened] = useState<boolean>(false);
   const { tasks = [], mutate } = useContext(TaskIndexContext);
-  const matchMutate = useMatchMutate();
 
   useEffect(() => {
     setConfirmDelete(false);
   }, [opened, modalOpened]);
 
-  const deleteTask = async () => {
-    if (!mutate) return console.error("No SWR mutate method found");
+  const deleteTask = async (): Promise<void> => {
+    if (!mutate) {
+      console.error("No SWR mutate method found");
+      return;
+    }
 
     setOpened(false);
-    await mutate(deleteProjectTask(projectId, id), {
-      optimisticData: tasks.filter((task) => id != task.id),
-      rollbackOnError: true,
-      populateCache: true,
-      revalidate: false,
-    });
-
-    await matchMutate(
-      task.sectionRoute,
-      () => getProjectSection(projectId, task.sectionId),
-      { revalidate: false }
-    );
+    const optimisticData = tasks.filter((task) => id !== task.id);
+    await deleteProjectTask(projectId, id);
+    await mutate(optimisticData, { optimisticData, populateCache: true });
   };
 
   return (
@@ -65,13 +56,17 @@ const TaskMenu = (props: TaskMenuProps) => {
       <Menu.Dropdown>
         <Menu.Label>Task options</Menu.Label>
         <Menu.Item
-          onClick={() => setModalOpened(true)}
+          onClick={() => {
+            setModalOpened(true);
+          }}
           icon={<IconInfoCircle size={16} />}
         >
           Info
         </Menu.Item>
         <Menu.Item
-          onClick={() => setEditMode(true)}
+          onClick={() => {
+            setEditMode(true);
+          }}
           color="blue"
           icon={<IconEdit size={16} />}
         >
@@ -80,17 +75,29 @@ const TaskMenu = (props: TaskMenuProps) => {
         <Menu.Divider />
         {confirmDelete ? (
           <Group position="center" py={4}>
-            <ActionIcon color="red" onClick={() => setOpened(false)}>
+            <ActionIcon
+              color="red"
+              onClick={() => {
+                setOpened(false);
+              }}
+            >
               <IconX size={16} />
             </ActionIcon>
 
-            <ActionIcon color="green" onClick={deleteTask}>
+            <ActionIcon
+              color="green"
+              onClick={() => {
+                void deleteTask();
+              }}
+            >
               <IconCheck size={16} />
             </ActionIcon>
           </Group>
         ) : (
           <Menu.Item
-            onClick={() => setConfirmDelete(true)}
+            onClick={() => {
+              setConfirmDelete(true);
+            }}
             closeMenuOnClick={false}
             color="red"
             icon={<IconTrash size={16} />}
