75ed3fd1808aa66b0b65fd282f4b245c1cbd2d98
kiizerd <ajsewell13@gmail.com>
2023-06-05 16:43:05 -0500

    Changed timeout default to 10 seconds.

--- src/modules/helpers/formHelpers.ts
+++ src/modules/helpers/formHelpers.ts
@@ -1,23 +1,33 @@
-import React from "react";
+import type React from "react";
 
 interface FormWithErrors {
-  clearErrors(): void;
+  clearErrors: () => void;
 }
 
-export const errorTimeout = (form: FormWithErrors, timeout?: number) => {
-  setTimeout(() => form.clearErrors(), timeout ? timeout : 5000)
+export const errorTimeout = (
+  form: FormWithErrors,
+  timeout: number = 10
+): void => {
+  timeout *= 1000;
+  setTimeout(() => {
+    form.clearErrors();
+  }, timeout);
 };
 
-export const flushTextAreaCursor = (e: React.FocusEvent<HTMLTextAreaElement, HTMLElement>) => {
+export const flushTextAreaCursor = (
+  e: React.FocusEvent<HTMLTextAreaElement, HTMLElement>
+): void => {
   const len = e.currentTarget.value.length;
   e.currentTarget.setSelectionRange(len, len);
-}
+};
 
-export const preventTextAreaSubmitOnEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-  if (e.key == "Enter" && !e.shiftKey) {
+export const preventTextAreaSubmitOnEnter = (
+  e: React.KeyboardEvent<HTMLTextAreaElement>
+): void => {
+  if (e.key === "Enter" && !e.shiftKey) {
     const target = e.target as HTMLTextAreaElement;
     const form = target.form;
     e.preventDefault();
     form?.requestSubmit();
   }
-}
+};
