7c0c6349893d3a2690ad1562a59336f3de586bd6
kiizerd <ajsewell13@gmail.com>
2023-06-04 23:50:03 -0500

    Removed stupid use of ref.

--- src/modules/common/components/TaskList/TaskList.tsx
+++ src/modules/common/components/TaskList/TaskList.tsx
@@ -1,23 +1,23 @@
-import { useContext, useRef } from "react";
-import { Box } from "@mantine/core";
+import { useContext } from "react";
+import { Box, Divider } from "@mantine/core";
 import TaskForm from "@forms/Task";
 import SectionContext from "@contexts/SectionContext";
 import TaskIndexContext from "@contexts/TaskIndexContext";
 import useTaskIndex from "@hooks/useTaskIndex";
 import TaskCard from "../TaskCard";
 
-const TaskList = () => {
+const TaskList = (): JSX.Element => {
   const section = useContext(SectionContext);
   const { tasks = [], mutate } = useTaskIndex(section.projectId, section.id);
-  const ref = useRef<HTMLDivElement | null>(null);
 
   return (
-    <TaskIndexContext.Provider value={{ tasks, mutate, ref }}>
-      <Box ref={ref} className="task-list">
+    <TaskIndexContext.Provider value={{ tasks, mutate }}>
+      <Box className="task-list">
         {tasks.map((task) => (
           <TaskCard key={task.id} task={task} />
         ))}
       </Box>
+      <Divider />
       <TaskForm ids={[section.projectId, section.id]} />
     </TaskIndexContext.Provider>
   );
