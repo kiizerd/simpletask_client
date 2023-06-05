23e422b2ab9a099fa84bbc539d68757cf4c2f848
kiizerd <ajsewell13@gmail.com>
2023-06-05 15:44:17 -0500

    Added error handling to Tasks API routes.

--- src/modules/api/tasks.ts
+++ src/modules/api/tasks.ts
@@ -1,6 +1,12 @@
 import { projectsRoute } from "./projects";
 import { sectionsRoute } from "./sections";
-import Task, { TaskData } from "types/Task";
+import Task, { type TaskData } from "types/Task";
+
+interface TaskErrorData {
+  code: string;
+  messages?: { name?: string[]; project?: string[]; section?: string[] };
+  details?: { name?: string; project?: string; section?: string };
+}
 
 const tasksRoute = (projectId: number): string =>
   projectsRoute + `/${projectId}/tasks`;
@@ -9,13 +15,19 @@ const getProjectTask = async (
   projectId: number,
   taskId: number
 ): Promise<Task> => {
-  const response = await fetch(tasksRoute(projectId) + `/${taskId}`, {
-    credentials: "include",
-  });
-  const data = (await response.json()) as TaskData;
-  const task = new Task(data.id, data);
+  try {
+    const response = await fetch(tasksRoute(projectId) + `/${taskId}`, {
+      credentials: "include",
+    });
+    const data = (await response.json()) as TaskData;
+    const task = new Task(data.id, data);
 
-  return task;
+    return task;
+  } catch (error) {
+    console.error(error);
+
+    return new Task(0, {});
+  }
 };
 
 const getSectionTasks = async (
@@ -23,27 +35,38 @@ const getSectionTasks = async (
   sectionId: number
 ): Promise<Task[]> => {
   const sectionTasksRoute = `${sectionsRoute(projectId)}/${sectionId}/tasks`;
-  const response = await fetch(sectionTasksRoute, { credentials: "include" });
-  const data = (await response.json()) as TaskData[];
-  const tasks = data.map((dataItem) => new Task(dataItem.id, dataItem));
+  try {
+    const response = await fetch(sectionTasksRoute, { credentials: "include" });
+    const data = (await response.json()) as TaskData[];
+    const tasks = data.map((dataItem) => new Task(dataItem.id, dataItem));
 
-  return tasks;
+    return tasks;
+  } catch (error) {
+    console.error(error);
+
+    return [];
+  }
 };
 
 const getProjectTasks = async (projectId: number): Promise<Task[]> => {
-  const response = await fetch(tasksRoute(projectId), {
-    credentials: "include",
-  });
-  const data = (await response.json()) as TaskData[];
-  const tasks = data.map((dataItem) => new Task(dataItem.id, dataItem));
+  try {
+    const response = await fetch(tasksRoute(projectId), {
+      credentials: "include",
+    });
+    const data = (await response.json()) as TaskData[];
+    const tasks = data.map((dataItem) => new Task(dataItem.id, dataItem));
 
-  return tasks;
+    return tasks;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 const createProjectTask = async (
   projectId: number,
   taskData: Partial<Task>
-): Promise<Task> => {
+): Promise<Task | TaskErrorData> => {
   const { name, details, sectionId } = taskData;
   const request = new Request(tasksRoute(projectId), {
     method: "POST",
@@ -51,17 +74,29 @@ const createProjectTask = async (
     credentials: "include",
     body: JSON.stringify({ task: { name, details, sectionId } }),
   });
-  const response = await fetch(request);
-  const data = (await response.json()) as TaskData;
-  const task = new Task(data.id, data);
+  let taskErrorData: TaskErrorData = { code: "" };
+  try {
+    const response = await fetch(request);
+    if (!response.ok) {
+      taskErrorData = (await response.json()) as TaskErrorData;
+      throw new Error(
+        `Failed to create project task: ${response.status} ${response.statusText}`
+      );
+    }
+    const data = (await response.json()) as TaskData;
+    const task = new Task(data.id, data);
 
-  return task;
+    return task;
+  } catch (error) {
+    console.error(error);
+    return taskErrorData;
+  }
 };
 
 const updateProjectTask = async (
   projectId: number,
   taskData: Task
-): Promise<Task> => {
+): Promise<Task | TaskErrorData> => {
   const { id, name, details, status } = taskData;
   const taskRoute = tasksRoute(projectId) + `/${id}`;
   const request = new Request(taskRoute, {
@@ -70,25 +105,44 @@ const updateProjectTask = async (
     credentials: "include",
     body: JSON.stringify({ task: { name, details, status } }),
   });
-  const response = await fetch(request);
-  const data = (await response.json()) as TaskData;
-  const task = new Task(data.id, data);
+  let taskErrorData: TaskErrorData = { code: "" };
+  try {
+    const response = await fetch(request);
 
-  return task;
+    if (!response.ok) {
+      taskErrorData = (await response.json()) as TaskErrorData;
+      throw new Error(
+        `Failed to update project task: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = (await response.json()) as TaskData;
+    const task = new Task(data.id, data);
+
+    return task;
+  } catch (error) {
+    console.error(error);
+    return taskErrorData;
+  }
 };
 
 const deleteProjectTask = async (
   projectId: number,
   taskId: number
-): Promise<Task[]> => {
+): Promise<Response | Error> => {
   const taskRoute = tasksRoute(projectId) + `/${taskId}`;
   const request = new Request(taskRoute, {
     method: "DELETE",
     credentials: "include",
   });
-  const response = await fetch(taskRoute, request);
-  const data = (await response.json()) as TaskData[];
-  const tasks = data.map((dataItem) => new Task(dataItem.id, dataItem));
+  try {
+    const response = await fetch(taskRoute, request);
+    if (!response.ok) throw new Error("Couldn't delete task.");
+    return response;
+  } catch (error) {
+    return error as Error;
+  }
+};
 
 const moveSectionTask = async (index: number, task: Task): Promise<void> => {
   const { id, sectionId, projectId } = task;
