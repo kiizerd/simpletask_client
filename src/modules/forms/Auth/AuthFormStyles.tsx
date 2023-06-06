2f42f3c86b3e90498b0cc4d95b1150bb3eafce0d
kiizerd <ajsewell13@gmail.com>
2023-06-05 23:36:15 -0500

    Used better image in Auth forms.

--- src/modules/forms/Auth/AuthFormStyles.tsx
+++ src/modules/forms/Auth/AuthFormStyles.tsx
@@ -1,20 +1,26 @@
 import { createStyles } from "@mantine/core";
+import { HEADER_HEIGHT } from "app/components/Header/HeaderStyles";
 
 const authFormStyles = createStyles((theme) => ({
   wrapper: {
-    minHeight: 900,
     backgroundSize: "cover",
-    backgroundImage:
-      "url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)",
+    backgroundImage: 'url(./wide/0.png)',
+    backgroundPosition: "bottom right",
+    position: "absolute",
+    top: HEADER_HEIGHT,
+    bottom: 0,
+    right: 0,
+    left: 0,
   },
 
   form: {
     borderRight: `1px solid ${
       theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
     }`,
-    minHeight: 900,
+    height: "100%",
     maxWidth: 450,
-    paddingTop: 80,
+    padding: theme.spacing.xl,
+    borderRadius: 0,
 
     [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
       maxWidth: "100%",
@@ -23,15 +29,7 @@ const authFormStyles = createStyles((theme) => ({
 
   title: {
     color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-  },
-
-  logo: {
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    width: 120,
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
+    fontFamily: `Greycliff CF, ${theme.fontFamily ?? "Arial"}`,
   },
 }));
 
