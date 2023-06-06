5f5ea112624130edb065412e693ef026aadeba22
kiizerd <ajsewell13@gmail.com>
2023-06-05 23:35:25 -0500

    Improved image fetching helper.

--- src/modules/helpers/imageHelpers.ts
+++ src/modules/helpers/imageHelpers.ts
@@ -1,26 +1,7 @@
-import image1 from "../../assets/AutumnForest.jpg";
-import image2 from "../../assets/DesertMountains.jpg";
-import image3 from "../../assets/CountryRoadStormy.jpg";
-import image4 from "../../assets/MossyCanyon.jpg";
-import image5 from "../../assets/VerticalHill.jpg";
+import imageCount from "imageCount";
 
-const gallery: string[] = [image1, image2, image3, image4, image5];
-
-// Globbing method seems to not work in production
-// Haven't tried with vite 4
-//
-// const images = import.meta.glob("../assets/**/*.jpg");
-// for (const imagePath in images) {
-//   images[imagePath]().then(() => {
-//     const imageURL = new URL(imagePath, import.meta.url);
-//     gallery.push(imageURL);
-//   });
-// }
-
-export default function getImage() {
-  return gallery[Math.floor(Math.random() * gallery.length)];
-}
-
-export function getAllImages() {
-  return gallery;
+export default function getImage(index?: number): string {
+  const randomNumber: number = Math.floor(Math.random() * (imageCount + 1));
+  const image = index ? index % imageCount : randomNumber;
+  return `./1k/${image}.png`;
 }
