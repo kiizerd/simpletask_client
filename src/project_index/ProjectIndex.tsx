93608d0a2b35b7c6112ecd5df89fb3ba4c42a279
kiizerd <ajsewell13@gmail.com>
2023-06-10 14:35:46 -0500

    PLEEEEEEASE WHY WONT CORS WORK.

--- src/project_index/ProjectIndex.tsx
+++ src/project_index/ProjectIndex.tsx
@@ -4,12 +4,13 @@ import NewProjectBtn from "./components/NewProjectBtn";
 import ProjectGrid from "./components/ProjectGrid";
 import { useContext } from "react";
 import UserContext from "@contexts/UserContext";
+import { Navigate } from "react-router-dom";
 
 const ProjectIndexPage = (): JSX.Element => {
   const { currentUser } = useContext(UserContext)
   const { projects, error, isLoading } = useProjectIndex();
 
-  if (!currentUser) return <>{[]}</>
+  if (!currentUser) return <Navigate to="/welcome" />
 
   if (isLoading) return <Loader />;
   if (error) throw error;
