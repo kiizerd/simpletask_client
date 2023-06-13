53160ac738d7cfbb76a6378b9cd856a03c6deb8c
kiizerd <ajsewell13@gmail.com>
2023-06-12 21:21:07 -0500

    Added error handling to authorization functions.

--- src/modules/api/auth.ts
+++ src/modules/api/auth.ts
@@ -1,12 +1,19 @@
 import type { User } from "types/models";
 import { apiURL } from "./api";
 
+export interface AuthError {
+  code: string;
+  messages?: { email?: string[]; password?: string[] };
+  details?: { email?: string[]; password?: string[] };
+  status?: number;
+  error?: Error;
+}
+
 const authFetch = async (url: string, body: object): Promise<Response> => {
   return await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
-    mode: 'cors',
     body: JSON.stringify(body),
   });
 };
@@ -14,42 +21,60 @@ const authFetch = async (url: string, body: object): Promise<Response> => {
 export const signIn = async (
   email: string,
   password: string
-): Promise<User | undefined> => {
+): Promise<User | AuthError> => {
   const signInUrl = `${apiURL}/users/sign_in`;
-  try {
-    const response = await authFetch(signInUrl, { user: { email, password } });
-    const data = await response.json();
-    const user: User = data.user;
+  const response = await authFetch(signInUrl, { user: { email, password } });
+  if (response.ok) {
+    const { user } = await response.json();
     return user;
-  } catch (error) {
-    console.error("Error signing in.\n", error);
+  } else {
+    const errorMsg = "API sign-in authorization failed.";
+    const error = await handleAuthError(response, errorMsg);
+    return error;
   }
 };
 
 export const signUp = async (
   email: string,
   password: string
-): Promise<User | undefined> => {
+): Promise<User | AuthError> => {
   const signUpUrl = `${apiURL}/users/`;
-  try {
-    const response = await authFetch(signUpUrl, { user: { email, password } });
-    const data = await response.json();
-    const user: User = data.user;
+  const response = await authFetch(signUpUrl, { user: { email, password } });
+  if (response.ok) {
+    const { user } = await response.json();
     return user;
-  } catch (error) {
-    console.error("Error registering.\n", error);
+  } else {
+    const errorMsg = "API registration failed.";
+    const error = await handleAuthError(response, errorMsg);
+    return error;
   }
 };
 
-export const signOut = async (): Promise<void> => {
+export const signOut = async (): Promise<undefined | AuthError> => {
   const signOutUrl = `${apiURL}/users/sign_out`;
-  try {
-    const response = await fetch(signOutUrl, {
-      method: "DELETE",
-      credentials: "include",
-    });
-    return await response.json();
-  } catch (error) {
-    console.error("Error signing out.\n", error);
+  const response = await fetch(signOutUrl, {
+    method: "DELETE",
+    credentials: "include",
+  });
+  if (!response.ok) {
+    const errorMsg = "API sign-out deauthorization failed.";
+    const error = await handleAuthError(response, errorMsg);
+    return error;
   }
+};
+
+const responseStatus = (response: Response): string =>
+  `Status: ${response.status} ${response.statusText}`;
+
+const handleAuthError = async (
+  response: Response,
+  customMsg?: string
+): Promise<AuthError> => {
+  const data = await response.json();
+  const message = customMsg ?? "API authorization failed.";
+  const error = new Error(`${message}\n\t${responseStatus(response)}`);
+  console.error(error);
+  const authError: AuthError = { code: "" };
+  Object.assign(authError, { ...data, error, status: response.status });
+  return authError;
 };
