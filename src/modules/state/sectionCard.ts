53f3b5c2c9da3395a951f310fde8a2d96375546f
kiizerd <ajsewell13@gmail.com>
2023-06-15 13:47:16 -0500

    Added new actions to SectionCard reducer.

--- src/modules/state/sectionCard.ts
+++ src/modules/state/sectionCard.ts
@@ -5,29 +5,37 @@ interface SafeSectionHookData extends SectionHookData {
   section: Section;
 }
 
+export type SortMethod = "default" | "status" | "name";
+
 export interface SectionCardState {
-  editMode: boolean;
+  sortMethod: SortMethod;
+  editMode: boolean | undefined;
   menuOpened: boolean;
   dragLocked: boolean;
   showComplete: boolean;
   sortMenuOpened: boolean;
+  showDeleteConfirm: boolean;
   sectionData: SafeSectionHookData;
 }
 
 export type Action =
+  | { type: "SET_SORT_METHOD"; payload: SortMethod }
   | { type: "SET_EDIT_MODE"; payload: boolean }
   | { type: "SET_MENU_OPENED"; payload: boolean }
   | { type: "SET_DRAG_LOCKED"; payload: boolean }
   | { type: "SET_SHOW_COMPLETE"; payload: boolean }
   | { type: "SET_SORT_MENU_OPENED"; payload: boolean }
+  | { type: "SET_SHOW_DELETE_CONFIRM"; payload: boolean }
   | { type: "SET_SECTION_DATA"; payload: SafeSectionHookData };
 
 const initialState: SectionCardState = {
-  editMode: false,
+  sortMethod: "default",
+  editMode: undefined,
   menuOpened: false,
   dragLocked: false,
   showComplete: true,
   sortMenuOpened: false,
+  showDeleteConfirm: false,
   sectionData: {
     section: new Section(0, {}),
     error: undefined,
@@ -38,6 +46,8 @@ const initialState: SectionCardState = {
 
 function reducer(state: SectionCardState, action: Action): SectionCardState {
   switch (action.type) {
+    case "SET_SORT_METHOD":
+      return { ...state, sortMethod: action.payload };
     case "SET_EDIT_MODE":
       return { ...state, editMode: action.payload };
     case "SET_MENU_OPENED":
@@ -48,6 +58,8 @@ function reducer(state: SectionCardState, action: Action): SectionCardState {
       return { ...state, showComplete: action.payload };
     case "SET_SORT_MENU_OPENED":
       return { ...state, sortMenuOpened: action.payload };
+    case "SET_SHOW_DELETE_CONFIRM":
+      return { ...state, showDeleteConfirm: action.payload };
     case "SET_SECTION_DATA":
       return { ...state, sectionData: action.payload };
     default:
